af58d276c01fe103a5481b6dc0806550ff9eb602
Akila De Silva <akiladesilva97@gmail.com>
2026-06-05 23:56:02 +0100

    Changes to home page

--- Averon-FE/src/Components/Processing/Processing.jsx
+++ Averon-FE/src/Components/Processing/Processing.jsx
@@ -9,7 +9,7 @@ import sorting_line from '../../assets/cinnamon ground.jpg';
 // NOTE: Adjust these image paths to match your actual local file names for the steps
 import grading from '../../assets/grading.jpg'; 
 import drying from '../../assets/drying.jpg';
-import packaging from '../../assets/bcg.JPG';
+import packaging from '../../assets/bcg.JPEG';
 
 const Processing = () => {
   return (
