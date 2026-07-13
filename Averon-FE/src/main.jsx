def79762d87f7cce2d6453983edaee5221769125
Akila De Silva <akiladesilva97@gmail.com>
2026-07-13 20:20:31 +0100

    seo optimising main.jsx

--- Averon-FE/src/main.jsx
+++ Averon-FE/src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import { HelmetProvider } from 'react-helmet-async' 
+import { createRoot, hydrateRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+
+const app = (
   <StrictMode>
-    {/* <HelmetProvider>  */}
+    <HelmetProvider>
       <App />
-    {/* </HelmetProvider> */}
-  </StrictMode>,
+    </HelmetProvider>
+  </StrictMode>
 )
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, app)
+} else {
+  createRoot(rootElement).render(app)
+}
